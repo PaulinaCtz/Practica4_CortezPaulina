ac594a0316dba91b9cc05929fbec353e8ef6d4a3
PaulinaCtz <paulina.cortez228557@potros.itson.edu.mx>
2023-02-21 21:30:16 -0700

    Menu antojitos - Práctica 5

--- app/src/main/java/mx/edu/itson/practica4/MenuActivity.kt
+++ app/src/main/java/mx/edu/itson/practica4/MenuActivity.kt
@@ -2,10 +2,19 @@ package mx.edu.itson.practica4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var btnAntojitos:Button = findViewById(R.id.btnantojitos) as Button
+
+        btnAntojitos.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
